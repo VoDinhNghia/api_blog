fcc0a46983133f08b296211d33cb64bf18b33eb1
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2021-08-22 11:19:36 +0700

    v1
    v1

--- src/controller/Crypto.js
+++ src/controller/Crypto.js
@@ -1,28 +1,28 @@
 const crypto = require('crypto');
 
 let CryptoPass = function(PassWord) {
-    const cipher = crypto.createCipher('aes128', 'blog api @12N');
+    const cipher = crypto.createCipher('aes128', '**********');
     var password = cipher.update(PassWord, 'utf8', 'hex');
     password += cipher.final('hex');
     return password
 }
 
 let DicipherPass = function(PassWord) {
-    const decipher = crypto.createDecipher('aes128', 'blog api @12N');
+    const decipher = crypto.createDecipher('aes128', '*********');
     var decrypted = decipher.update(PassWord, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted
 }
 
-let ReplaceCharScriptFromForm = function(str_input) {
-    let new_str = '';
-    let new_str2 = '';
-    if (str_input.indexOf('<script>') != -1 || str_input.indexOf('</script>') != -1) {
-        new_str = str_input.replace('<script>', '%2rip%2');
-        new_str2 = new_str.replace('</script>', '%2rip%2:))');
-        return new_str2
+let ReplaceCharScriptFromForm = function(stringInput) {
+    let newString1 = '';
+    let newString2 = '';
+    if (stringInput.indexOf('<script>') != -1 || stringInput.indexOf('</script>') != -1) {
+        newString1 = stringInput.replace('<script>', '%2rip%2');
+        newString2 = newString1.replace('</script>', '%2rip%2:))');
+        return newString2
     } else {
-        return str_input
+        return stringInput
     }
 }
 
